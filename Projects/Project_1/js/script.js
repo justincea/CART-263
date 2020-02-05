41e7f293e205dc8c41c10689d9e5566cd23ff71c
Justin Luc Maxim Cea <justin.cea@yahoo.com>
2020-02-04 21:04:11 -0500

    P1: Added Messages & More
    - Added an array box w/ random messages
    - Made the Messages appear at random
    - Changed background Color

--- Projects/Project_1/js/script.js
+++ Projects/Project_1/js/script.js
@@ -5,8 +5,26 @@ Project 1
 Justin Cea
 *********************************************************************/
 let $chest;
-let $droppablecoin;
+let $coin;
+let $coin2;
+let $coin3;
+let $coin4;
+
 let $revertingcoin;
+let $revertingcoin2;
+
+let random;
+let message = [
+  "Why are all the coins out of the chest?!",
+  "Put the coins back where you found them",
+  "Are you trying to steal the coins?",
+  "How does it feel to have these coins?",
+  "How do you feel?",
+  "Have you finished putting them away?",
+  "What are you doing Sisyphus?",
+  "I thought you wanted to collect these coins?",
+  "Don't you want to be wealthy?"
+];
 
 let coinSFX = new Audio("assets/sounds/coinSound.mp3");
 
@@ -17,19 +35,32 @@ function setup() {
   $chest.droppable({
     drop: onDrop
   });
-  $droppablecoin = $("#droppablecoin");
-  $droppablecoin.draggable();
-
   $revertingcoin = $("#revertingcoin");
   $revertingcoin.draggable({
     revert: "valid"
   });
+
+  $revertingcoin2 = $("#revertingcoin2");
+  $revertingcoin2.draggable({
+    revert: "valid"
+  });
+  $coin = $("#coin");
+  $coin.draggable();
+
+  $coin2 = $("#coin2");
+  $coin2.draggable();
+
+  $coin3 = $("#droppablecoin3");
+  $coin3.draggable();
+
+  $coin4 = $("#droppablecoin4");
+  $coin4.draggable();
 }
 
-function onDrop() {
+function onDrop(event, ui) {
   console.log("Dropped");
   $droppablecoin.remove();
-}
-function revertingcoinDrop() {
-  $revertingcoin.draggable();
+
+  $("#dialogues").text(message[random]);
+  random = Math.floor(Math.random() * message.length);
 }
