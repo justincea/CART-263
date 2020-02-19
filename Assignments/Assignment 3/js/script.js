bb5eac7d29abedeeb3e9ac6b1aec4c064d629242
Justin Luc Maxim Cea <justin.cea@yahoo.com>
2020-02-18 19:06:44 -0500

    A3: Added Beginning Comments

--- Assignments/Assignment 3/js/script.js
+++ Assignments/Assignment 3/js/script.js
@@ -10,7 +10,7 @@ Justin Cea
 *********************************************************************/
 
 $(document).ready(setup);
-
+// An array of animal names that we use to create our guessing game
 let animals = [
   "aardvark",
   "alligator",
@@ -147,33 +147,57 @@ let animals = [
   "yak",
   "zebra"
 ];
-
+// We also track the set of answers
 let answers = [];
+// We need to track the correct button for each round
 let correctAnimal;
-const NUM_OPTIONS = 5;
+// How many possible answers there are per round
 
+const NUM_OPTIONS = 5;
+// setup()
+//
+// We just start a new round right away!
 function setup() {
   newRound();
 }
-
+// addButton(label)
+//
+// Creates a button using jQuery UI on a div with the label specified
+// and adds it to the page, returning the button as well
 function addButton(label) {
+  // Create a div with jQuery using HTML
   let $div = $("<div></div>");
+  // Give it the guess class
   $div.addClass("guess");
+  // Set the text in the div to our label
   $div.text(label);
+  // Turn the div into a button using jQuery UI's .button() method
   $div.button();
+  // Adds the button to the page so we can see it
   $div.appendTo("body");
+  // Listen for a click on the button which means the user has guessed
   $div.on("click", handleGuess);
+  // Return the button
 }
-
+// newRound()
+//
+// Generates a set of possible answers randomly from the set of animals
+// and adds buttons for each one. Then chooses the correct button randomly.
 function newRound() {
   answers = [];
   console.log(NUM_OPTIONS);
+  // Loop for each option we'll offter
   for (var i = 0; i < NUM_OPTIONS; i++) {
+    // Choose the answer text randomly from the animals array
     let answer = animals[Math.floor(Math.random() * animals.length)];
+    // Add a button with this label
     addButton(answer);
+    // Add this button to the buttons array
     answers.push(answer);
   }
+  // Choose a random button from the buttons array as our correct button
   correctAnimal = answers[Math.floor(Math.random() * answers.length)];
+  // Say the label (text) on this button
   sayBackwards(correctAnimal);
 }
 
@@ -186,17 +210,31 @@ function handleGuess() {
     $(this).effect("shake");
   }
 }
-
+// sayBackwards(text)
+//
+// Uses ResponsiveVoice to say the specified text backwards!
 function sayBackwards(text) {
+  // We create a reverse version of the name by:
+  // 1. using .split('') to split the string into an array with each character
+  // as a separate element.
+  // e.g. "bat" -> ['b','a','t']
+  // 2. using .reverse() on the resulting array to create a reverse version
+  // e.g. ['b','a','t'] -> ['t','a','b']
+  // 3. using .join('') on the resulting array to create a string version of the array
+  // with each element forming the string (joined together with nothing in between)
+  // e.g. ['t','a','b'] -> "tab"
+  // (We do this all in one line using "chaining" because .split() returns an array for
+  // for .reverse() to work on, and .reverse() returns an array for .join() to work on.)
   let backwardsText = text
     .split("")
     .reverse()
     .join("");
-
+  // Set some random numbers for the voice's pitch and rate parameters for a bit of fun
   let options = {
     pitch: Math.random(),
     rate: Math.random()
   };
-
+  // Use ResponsiveVoice to speak the string we generated, with UK English Male voice
+  // and the options we just specified.
   responsiveVoice.speak(backwardsText, "UK English Male", options);
 }
