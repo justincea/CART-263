4a407171504a32bd48b82c678db8c52a7b9a4543
Justin Luc Maxim Cea <justin.cea@yahoo.com>
2020-03-04 17:05:51 -0500

    P3: added responsiveVoice

--- Projects/Project_2/js/script.js
+++ Projects/Project_2/js/script.js
@@ -56,10 +56,11 @@ if (annyang) {
   annyang.start();
 }
 
-function setup() {}
-$("#egg").click(function() {
-  console.log("image is clicked");
-});
+function setup() {
+  responsiveVoice.speak(
+    "Welcome to the world's #1 video hosting service! Feel free to watch some entertaining videos, followed by the list below.!"
+  );
+}
 function peppaPig() {
   console.log("peppa");
   $(".appearImages").remove();
@@ -67,6 +68,7 @@ function peppaPig() {
     '<img id="peppa"class = "appearImages" src="assets/images/peppa_pig.png">'
   ).appendTo("h1");
   console.log($(".appearImages"));
+  responsiveVoice.speak("are you... entertained?");
 }
 function elsa() {
   $(".appearImages").remove();
@@ -74,13 +76,15 @@ function elsa() {
     '<img id= "elsa" class = "appearImages"src="assets/images/elsa.png">'
   ).appendTo("h1");
   console.log("elsa");
+  responsiveVoice.speak("isn't this just... lovely.");
 }
 function mickeyMouse() {
   $(".appearImages").remove();
   $(
     '<img id= "mM" class = "appearImages"src="assets/images/mickey_mouse.jpg">'
-  ).appendTo("h3");
+  ).appendTo("h12");
   console.log("mickey");
+  responsiveVoice.speak("is this... what you're looking for?");
 }
 function minnieMouse() {
   $(".appearImages").remove();
@@ -88,6 +92,9 @@ function minnieMouse() {
     '<img id="mMe" class = "appearImages"src="assets/images/minnie_mouse.png">'
   ).appendTo("h4");
   console.log("minnie");
+  responsiveVoice.speak(
+    "it seems that baby mickey is pretending to die, that sounds like a fun prank idea to pull on our parents, don't you think?"
+  );
 }
 function spiderman() {
   $(".appearImages").remove();
@@ -95,6 +102,9 @@ function spiderman() {
     '<img id="spiderman" class = "appearImages"src="assets/images/spiderman.gif">'
   ).appendTo("h5");
   console.log("spiderman");
+  responsiveVoice.speak(
+    "looks like they're having lots of fun! I would definetly love to try that one day"
+  );
 }
 function masha() {
   $(".appearImages").remove();
@@ -102,6 +112,9 @@ function masha() {
     '<img id= "masha" class = "appearImages"src="assets/images/masha.png">'
   ).appendTo("h6");
   console.log("masha");
+  responsiveVoice.speak(
+    "Hillarious. Wouldn't it be fun to switch faces with someone for a day?"
+  );
 }
 
 function pony() {
@@ -109,12 +122,16 @@ function pony() {
   $(
     '<img id= "pony" class = "appearImages" src="assets/images/pony.gif">'
   ).appendTo("h7");
+  responsiveVoice.speak(
+    "I want one of those in my room, do u want me to search on the web for a my little pony synonymous to the following?!!"
+  );
 }
 function spongebob() {
   $(".appearImages").remove();
   $(
     '<img id= "spongebob" class = "appearImages"src="assets/images/spongebob.gif">'
   ).appendTo("h8");
+  responsiveVoice.speak("I wish I can do that with my lashes");
 }
 
 function baby() {
@@ -122,16 +139,31 @@ function baby() {
   $(
     '<img id= "baby" class = "appearImages"src="assets/images/baby.png">'
   ).appendTo("h9");
+  responsiveVoice.speak(
+    "I want to drink what he's drinking, it seems delightful"
+  );
 }
 function daveEva() {
   $(".appearImages").remove();
   $('<img id= "da" class = "appearImages"src="assets/images/da.png">').appendTo(
     "h10"
   );
+  responsiveVoice.speak("a monkey family... how wonderful...");
 }
 function fingerFamily() {
   $(".appearImages").remove();
   $(
     '<img id= "fingerFamily" class = "appearImages"src="assets/images/finger_family.gif">'
   ).appendTo("h11");
+  responsiveVoice.speak(
+    "I also want to cut my family and put them on ...my fingers..., would you like me to search on the web on ways to cut your family and put them on your fingers??"
+  );
+}
+
+function johnny() {
+  $(".appearImages").remove();
+  $(
+    '<img id= "johnny" class = "appearImages"src="assets/images/johnny.jpg">'
+  ).appendTo("h12");
+  responsiveVoice.speak("Their dance moves are exquisite");
 }
