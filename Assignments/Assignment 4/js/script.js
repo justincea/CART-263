9555d400aaf4034e71b80c80d753ab988c14b9ca
Justin Luc Maxim Cea <justin.cea@yahoo.com>
2020-03-10 19:02:58 -0400

    A4: Descriptions & Determiners
    - Added two descriptive elements to the condiment from the corpora. (adverbs and sausages)
    -  Checks if the word following an indefinite article starts with a vowel and use 'an' instead of 'a'.
    - Allows the user to reset the page when the mouse is clicked.

--- Assignments/Assignment 4/js/script.js
+++ Assignments/Assignment 4/js/script.js
@@ -16,26 +16,47 @@ function setup() {
   $.getJSON("data/data.json")
     .done(dataLoaded)
     .fail(dataNotLoaded);
+  $(document).on("click", mousePressed);
 }
 
 function dataLoaded(data) {
   console.log(data);
   let randomCondiment = getRandomArrayElement(data.condiments);
   console.log(randomCondiment);
-
+  //
   let verb = "is";
   if (randomCondiment.charAt(randomCondiment.length - 1) === "s") {
     verb = "are";
   }
   console.log(verb);
+
+  let determiners = "a";
+  if (
+    randomCondiment.charAt(0) === "A" ||
+    randomCondiment.charAt(0) === "E" ||
+    randomCondiment.charAt(0) === "I" ||
+    randomCondiment.charAt(0) === "O" ||
+    randomCondiment.charAt(0) === "U"
+  ) {
+    determiners = "an";
+
+    console.log(randomCondiment.charAt(0));
+  }
+  //
   let randomCat = getRandomArrayElement(data.cats);
   console.log(randomCat);
-
+  //
   let randomRoom = getRandomArrayElement(data.rooms);
   console.log(randomRoom);
-
-  let randomDescription = `${randomCondiment} ${verb} ${randomCat} ${randomRoom}`;
-  $("body").append(randomDescription);
+  //
+  let randomSausage = getRandomArrayElement(data.sausages);
+  console.log(randomSausage);
+  //
+  let randomAdverb = getRandomArrayElement(data.adverbs);
+  console.log(randomAdverb);
+  //
+  let randomDescription = `${randomCondiment} and ${randomSausage} ${verb} ${randomAdverb} like ${determiners} ${randomCat} ${randomRoom}`;
+  $("body").text(randomDescription);
 }
 
 function dataNotLoaded(request, text, error) {
@@ -45,4 +66,10 @@ function dataNotLoaded(request, text, error) {
 function getRandomArrayElement(array) {
   let element = array[Math.floor(Math.random() * array.length)];
   return element;
+}
+
+function mousePressed() {
+  $.getJSON("data/data.json", dataLoaded);
+
+  console.log("mouseIsPressed");
 }
